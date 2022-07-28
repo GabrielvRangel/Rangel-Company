f1bdd4baa6ef1da952c6bb7a8dd823c65daa5b95
Gabriel Rangel <gabrielrangel.001@gmail.com>
2022-07-28 15:18:19 -0300

    vídeo de fundo + nova foto apresentação

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Rangel
   class Application < Rails::Application
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
