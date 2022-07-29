6ad86841db9c1aea043d2de965f72aeea86e364e
Gabriel Rangel <gabrielrangel.001@gmail.com>
2022-07-28 23:20:38 -0300

    teste em produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,14 +119,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   #email
   config.action_mailer.default_url_options = { host: 'rangel-company.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-	  address: 'smtp.gmail.com',
-	  port: 587,
-	  domain: 'gmail.com',
-	  user_name: Rails.application.credentials.gmail[:user_name].to_s,
-	  password: Rails.application.credentials.gmail[:password],
-	  authentication: 'plain',
-	  emable_starttls_auto: true	
-  }
+  
 end
